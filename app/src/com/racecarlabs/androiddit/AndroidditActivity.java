fb89252f9b82512450c423ad2738bc65fd09e378
Kevin Mateo Lim <kevin@racecarlabs.com>
2012-05-25 11:03:58 -0700

    initial commit: boilerplate android app

---
+++ app/src/com/racecarlabs/androiddit/AndroidditActivity.java
@@ -0,0 +1,13 @@
+package com.racecarlabs.androiddit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidditActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
