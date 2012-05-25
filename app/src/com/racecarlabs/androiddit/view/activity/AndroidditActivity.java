e66417227051aa0c8ceb6dc677ce66f2d50140a9
Kevin Mateo Lim <kevin@racecarlabs.com>
2012-05-25 11:30:15 -0700

    reorg activity into view package

--- app/src/com/racecarlabs/androiddit/view/activity/AndroidditActivity.java
+++ app/src/com/racecarlabs/androiddit/view/activity/AndroidditActivity.java
@@ -1,4 +1,7 @@
-package com.racecarlabs.androiddit;
+package com.racecarlabs.androiddit.view.activity;
+
+import com.racecarlabs.androiddit.R;
+import com.racecarlabs.androiddit.R.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
